c4eb4e5c91c1ddc2957aee1d2cd5f03672e5807e
Vasily Kuzin <exer7um@gmail.com>
2023-02-14 12:02:49 +0300

    Добавление конфигурации Prettier #3

---
+++ index.js
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  jsonRecursiveSort: true,
+  plugins: [
+    require("prettier-plugin-sort-json"),
+    require("prettier-plugin-css-order"),
+    require("prettier-plugin-packagejson"),
+    require("prettier-plugin-tailwindcss"),
+  ],
+  semi: false,
+  singleQuote: false,
+}
