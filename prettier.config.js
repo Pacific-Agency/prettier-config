f047acf33dc2624a3ecd7e4bdf3408a628f6802c
Vasily Kuzin <exer7um@gmail.com>
2023-07-20 13:29:45 +0300

    Выставление правила на ignore #28

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,6 @@
 /** @type {import('prettier').Config} */
 module.exports = {
+  htmlWhitespaceSensitivity: "ignore",
   plugins: [require("prettier-plugin-packagejson")],
   semi: false,
   singleQuote: false,
